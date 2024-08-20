091268ac1a44425304c0d7f7c0001ae971107857
Juliana Melo <136100085+julianafhm@users.noreply.github.com>
2024-08-20 11:02:47 -0300

    Update Prova.cs

--- periodoAcademico/Models/Prova.cs
+++ periodoAcademico/Models/Prova.cs
@@ -2,7 +2,7 @@
     public class Prova 
     {
     public int Id { get; set; }
-    public int Numero { get; set; } // 1, 2 ou 3
+    public int Avaliacao { get; set; } //av1,av2,av3 ou av4(P. ESP)
     public double Nota { get; set; }
     public int AlunoId { get; set; }
     public Aluno Aluno { get; set; } = new Aluno();
