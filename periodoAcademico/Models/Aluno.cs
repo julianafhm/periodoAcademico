48e3a8df56add23cecc2f522019df0e386e4f3fc
Juliana Melo <136100085+julianafhm@users.noreply.github.com>
2024-08-20 11:15:33 -0300

    Update Aluno.cs

--- periodoAcademico/Models/Aluno.cs
+++ periodoAcademico/Models/Aluno.cs
@@ -6,5 +6,6 @@
     public List<Prova> Provas { get; set; } = new List<Prova>();
     public double MediaFinal { get; set; }
     public double? NotaProvaFinal { get; set; }
+    public double? NotaProvaEspecial { get; set; }
     public bool Aprovado { get; set; }
 }
