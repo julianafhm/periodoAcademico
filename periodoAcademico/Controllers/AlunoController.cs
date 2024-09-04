f926c953499df88887233ba67a5aed46d2909b4d
Juliana Melo <ferreirajuliana01234@gmail.com>
2024-09-04 12:18:21 -0300

    update

--- periodoAcademico/Controllers/AlunoController.cs
+++ periodoAcademico/Controllers/AlunoController.cs
@@ -6,32 +6,37 @@ using periodoAcademico.CQRS.Queries.Alunos.Listar;
 
 
 
-namespace periodoAcademico.Controllers {
+namespace periodoAcademico.Controllers
+{
 
     [Route("api/aluno")]
     [Produces("application/json")]
     [ApiController]
-    public class AlunoController : ControllerBase {
+    public class AlunoController : ControllerBase
+    {
 
         private readonly IMediator mediator;
 
-        public AlunoController(IMediator mediator) {
+        public AlunoController(IMediator mediator)
+        {
 
             this.mediator = mediator;
         }
         [HttpGet("list")]
-        public async Task<IActionResult> Create([FromQuery] ListarAlunoQuery command) {
+        public async Task<IActionResult> Create([FromQuery] ListarAlunoQuery command)
+        {
             return Created(string.Empty, await this.mediator.Send(command));
         }
 
         [HttpPost("create")]
-        public async Task<IActionResult> Create([FromBody] CriarAlunoCommand command) {
+        public async Task<IActionResult> Create([FromBody] CriarAlunoCommand command)
+        {
             return Created(string.Empty, await this.mediator.Send(command));
         }
 
-        
+
 
     }
 
-         
+
 }
